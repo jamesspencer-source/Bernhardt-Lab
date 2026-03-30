b51c5083d8776e186bafe9b62dd6e0a2cf17061d
jamesspencer-source <jmspencer1996@gmail.com>
2026-03-30 09:07:17 -0400

    site: sync github-flat asset mirror

--- github-flat/assets/alumni 2.js
+++
@@ -1,9 +0,0 @@
-import { initSiteCore } from "./js/site-core.js";
-import { initAlumniDirectory } from "./js/alumni-directory.js";
-
-async function initializeAlumniPage() {
-  await initSiteCore();
-  initAlumniDirectory();
-}
-
-initializeAlumniPage();
